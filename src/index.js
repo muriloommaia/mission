9bfe78e4fc09d984e83ce7ecdade3b24a57f6193
Murilo <muriloommaia@gmail.com>
2021-06-04 17:51:22 -0300

    site

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
-
+import '../node_modules/font-awesome/less/variables.less'
 
 ReactDOM.render(
   <React.StrictMode>
